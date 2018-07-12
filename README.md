4ccb125cc99a991f1b6c4395abd1b83c3661fa90
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-07-12 00:42:06 -0300

    Agrego template del parcial. Los alumnos deben completarlo.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# 2018-l-mascotasChapita
-Repositorio base para el parcial de lógico "Mascotas Chapita"
+# Parcial Lógico "Mascotas Chapita"
+
+- Enunciado [acá](https://docs.google.com/document/d/1TBECVzcWWOeWUAfvHXMUETh-aUUIQYdPz45xKXjcOpQ/edit)
+
+## Importante:
+
+- El parcial debe hacerse **en el archivo provisto** "mascotasChapita.pl".
+- Dentro debe completarse **el nombre y curso** del alumno.
+
---
+++ mascotasChapita.pl
@@ -0,0 +1,23 @@
+% Apellido y Nombre: 
+% Curso:
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Se tienen los siguientes functores:
+% perro(tamaño)
+% gato(sexo, cantidad de personas que lo acariciaron)
+% tortuga(carácter)
+
+mascota(pepa, perro(mediano)).
+mascota(frida, perro(grande)).
+mascota(piru, gato(macho,15)).
+mascota(kali, gato(hembra,3)).
+mascota(olivia, gato(hembra,16)).
+mascota(mambo, gato(macho,2)).
+mascota(abril, gato(hembra,11)).
+mascota(buenaventura, tortuga(agresiva)).
+mascota(severino, tortuga(agresiva)).
+mascota(simon, tortuga(tranquila)).
+mascota(quinchin, gato(macho,0)).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
